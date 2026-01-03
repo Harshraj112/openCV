dec11aa98ca11b46a62bcf4985a15560618edb47
HARSH RAJ <harshraj250106@gmail.com>
2026-01-04 00:23:48 +0530

    Colour detection

---
+++ Colour-detection/green-colour-detection.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([35, 50, 50])
+    upper_green = np.array([85, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("Original", frame)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", result)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
